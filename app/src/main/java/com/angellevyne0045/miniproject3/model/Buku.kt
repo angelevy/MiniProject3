30b8ba6a92b66be3a34a984f4353968b221ef857
Angel Levyne <angellevyne@gmail.com>
2025-06-06 03:33:33 +0700

    Memproses data string sebagai JSON

---
+++ app/src/main/java/com/angellevyne0045/miniproject3/model/Buku.kt
@@ -0,0 +1,7 @@
+package com.angellevyne0045.miniproject3.model
+
+data class Buku(
+    val title: String,
+    val author: String,
+    val coverUrl: String
+)
--- app/src/main/java/com/angellevyne0045/miniproject3/network/BukuApiService.kt
+++ app/src/main/java/com/angellevyne0045/miniproject3/network/BukuApiService.kt
@@ -1,20 +1,27 @@
 package com.angellevyne0045.miniproject3.network
 
+import com.angellevyne0045.miniproject3.model.Buku
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 
 private const val BASE_URL = "https://raw.githubusercontent.com/angelevy/booksMobproAngel/refs/heads/main/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface BukuApiService {
     @GET("bookss.json")
-    suspend fun getBuku(): String
+    suspend fun getBuku(): List<Buku>
 }
 
 object BukuApi {
