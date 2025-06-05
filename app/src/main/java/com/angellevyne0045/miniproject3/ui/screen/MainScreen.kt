9b269c5809952af5d956f01f4af02a897a5de541
Angel Levyne <angellevyne@gmail.com>
2025-06-06 03:52:14 +0700

    Membuat tampilan title dan author buku

--- app/src/main/java/com/angellevyne0045/miniproject3/ui/screen/MainScreen.kt
+++ app/src/main/java/com/angellevyne0045/miniproject3/ui/screen/MainScreen.kt
@@ -1,8 +1,10 @@
 package com.angellevyne0045.miniproject3.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -17,12 +19,16 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.angellevyne0045.miniproject3.R
@@ -69,7 +75,8 @@ fun ScreenContent(modifier: Modifier = Modifier){
 @Composable
 fun ListItem(buku: Buku) {
     Box(
-        modifier = Modifier.padding(4.dp).border(1.dp, Color.Gray)
+        modifier = Modifier.padding(4.dp).border(1.dp, Color.Gray),
+        contentAlignment = Alignment.BottomCenter
     ){
         AsyncImage(
             model = buku.coverUrl,
@@ -77,6 +84,23 @@ fun ListItem(buku: Buku) {
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxWidth().padding(4.dp)
         )
+        Column(
+                modifier = Modifier.fillMaxWidth().padding(4.dp)
+            .background(Color(red = 0f, green = 0f, blue = 0f, alpha = 0.5f))
+            .padding(4.dp)
+        ) {
+            Text(
+                text = buku.title,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            Text(
+                text = buku.author,
+                fontStyle = FontStyle.Italic,
+                fontSize = 14.sp,
+                color = Color.White
+            )
+        }
     }
 }
 
