a8276785d854cf1ef01d6197124b28745cd305d3
Angel Levyne <angellevyne@gmail.com>
2025-06-07 02:24:54 +0700

    Membuat dialog tambah buku

--- app/src/main/java/com/angellevyne0045/miniproject3/ui/screen/MainScreen.kt
+++ app/src/main/java/com/angellevyne0045/miniproject3/ui/screen/MainScreen.kt
@@ -88,10 +88,12 @@ fun MainScreen() {
     val user by dataStore.userFlow.collectAsState(User())
 
     var showDialog by remember { mutableStateOf(false) }
+    var showBukuDialog by remember { mutableStateOf(false) }
 
     var bitmap: Bitmap? by remember { mutableStateOf(null) }
     val launcher = rememberLauncherForActivityResult(CropImageContract()) {
         bitmap = getCroppedImage(context.contentResolver, it)
+        if (bitmap != null ) showBukuDialog = true
     }
 
     Scaffold (
@@ -149,6 +151,14 @@ fun MainScreen() {
                 onDismissRequest = {showDialog = false}) {
                 CoroutineScope(Dispatchers.IO).launch {signOut(context, dataStore)}
                 showDialog = false
+            }
+        }
+        if (showBukuDialog) {
+            BukuDialog (
+                bitmap = bitmap,
+                onDismissRequest = { showBukuDialog = false }) { title, author ->
+                Log.d("TAMBAH", "$title dari $author ditambahkan.")
+                showBukuDialog = false
             }
         }
     }
